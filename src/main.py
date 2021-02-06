18123a89e2c33911d61593208da7c7928b0a798d
kevin <kevin.vandenbreemen@gmail.com>
2021-02-05 23:07:01 -0500

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,5 @@
+version = "1.0.0"
+softwareName = f"""Multi-Platform Game Engine in Python
+Written by Kevin R. VanDenBreemen
+Version {version}"""
+print (softwareName)
---
+++ src/tests/test_commands.py
@@ -0,0 +1,9 @@
+import unittest
+
+class CommandTest(unittest.TestCase):
+
+    def test_handleCommand(self):
+        print('testing')
+
+if __name__ == '__main__':
+    unittest.main()
